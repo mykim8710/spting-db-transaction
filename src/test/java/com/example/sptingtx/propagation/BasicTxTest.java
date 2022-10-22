5131c8ee1ee0334904aee690d8905b76bc3e07da
김민영 <mykim8710@gmail.com>
2022-10-22 19:59:52 +0900

    [ Study ] Spring - db - Transaction

--- src/test/java/com/example/sptingtx/propagation/BasicTxTest.java
+++ src/test/java/com/example/sptingtx/propagation/BasicTxTest.java
@@ -128,4 +128,23 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 커밋");
         Assertions.assertThatThrownBy(() -> txManager.commit(outer)).isInstanceOf(UnexpectedRollbackException.class);
     }
+    
+    @Test
+    void innerRollbackRequiresNewTest() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner); //롤백
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer); //커밋
+    }
 }
